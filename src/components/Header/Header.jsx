149bfd845e478a641cf9f112dde10fc4cd4c8e22
itsrerodrigs <itsrerodrigs@gmail.com>
2024-11-02 13:06:57 -0300

    caminho header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import style from "./Header.module.css";
+import style from "../Header/Header.module.css";
 import img from "../../assets/Logo1.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
