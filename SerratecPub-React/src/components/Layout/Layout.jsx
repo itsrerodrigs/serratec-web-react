5904fc553c6c92cff3b3f523c17535ceb6d14062
isb-op <isabella.op95@gmail.com>
2024-11-04 15:49:41 -0300

    home

--- SerratecPub-React/src/components/Layout/Layout.jsx
+++ SerratecPub-React/src/components/Layout/Layout.jsx
@@ -1,9 +1,12 @@
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
+import { HomePage } from "../../pages/Home/HomePage";
+
 
 export function Layout(){
     return(
         <>
+        <HomePage/>
         </>
     )
 }
